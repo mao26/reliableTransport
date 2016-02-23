3fdfc672f1ee1f18068e5b3b4aa3e8acc3ba59e0
John Dai <johnrundai@gmail.com>
2016-02-23 14:43:34 -0500

    worked on sender side with reference

--- reliable.c
+++ reliable.c
@@ -19,6 +19,9 @@ static int quit = 0;
 void iterPackNAdd(packet_t * pack, rel_t * s);
 static int counter = 0;
 static packet_t* windowfull = NULL;
+static int eofsent=0;
+static int eofseqno=0;
+static int eofacked=0;
 
 struct packetnode {
 	int length;
@@ -129,16 +132,16 @@ rel_create (conn_t *c, const struct sockaddr_storage *ss,
 void
 rel_destroy (rel_t *r)
 {
-	if (r->next)
-		r->next->prev = r->prev;
-	*r->prev = r->next;
+	//fprintf(stderr,"rel_destroy\n");
+	//if (r->next)
+	//	r->next->prev = r->prev;
+	//*r->prev = r->next;
 	conn_destroy (r->c);
 
-	//fprintf(stderr,"rel_destroy\n");
+
 	/* Free any other allocated memory here */
 	free(r->senderbuffer);
 	free(r->receiverbuffer);
-	free(r->packet);
 }
 
 /* Signal Handler for SIGINT */
@@ -208,7 +211,7 @@ int iter_PackNAdd(packet_t * pack, rel_t * s)
 		current = current -> next;
 		count++;
 	}
-	//fprintf(stderr, "last frame sent: %d", s->send_sw->lfs);
+
 	if (count >= s->send_sw->sws) {
 		windowfull=pack;
 		return 0;
@@ -223,6 +226,7 @@ int iter_PackNAdd(packet_t * pack, rel_t * s)
 	else {
 		prev->next = current;
 	}
+	//fprintf(stderr, "packnadd: %s", pack->data);
 	conn_sendpkt(s->c, pack, sizeof(packet_t));
 	s->send_sw->lfs = ntohl(pack->seqno);
 	return 1;
@@ -279,12 +283,17 @@ rel_demux (const struct config_common *cc,
 }
 
 void
-rel_sendack(rel_t *r) {
+rel_sendack(rel_t *r, int iseof) {
 	//fprintf(stderr,"sendack");
 	packet_t* ackpack = malloc(sizeof(packet_t));
 	ackpack->cksum = 0;
 	//r->acknum++; //Not sure if this is necessary
+	//fprintf(stderr,"rec_read:%d",ntohl(r->rec_sw->head->packet->seqno));
 	ackpack->ackno = htonl((r->seqNumToAck));
+	if (iseof==1 && r->rec_sw->head->next==NULL) {
+		//fprintf(stderr,"rec_readddddddddddddddddd:%d",r->seqNumToAck);
+		ackpack->ackno = htonl((r->seqNumToAck)+1);
+	}
 	ackpack->len = htons(8); //not sure if this is correct
 	ackpack->cksum = cksum(ackpack, ntohs(ackpack->len));
 	conn_sendpkt(r->c, ackpack, sizeof(packet_t));
@@ -292,6 +301,7 @@ rel_sendack(rel_t *r) {
 void
 rel_sendeof(rel_t *r) {
 	//fprintf(stderr,"UPDATE THIS");
+	eofseqno = r->seqnum;
 	packet_t* eofpack = malloc(sizeof(packet_t));
 	eofpack->cksum = 0;
 	eofpack->ackno = htonl(r->acknum);
@@ -330,15 +340,25 @@ rel_recvpkt (rel_t *r, packet_t *pkt, size_t n)
 		//fprintf(stderr, "dropped");
 		return;
 	}
+
 	else if (ntohs(pkt->len) == 8) {
 		//fprintf(stderr,"ackkkkkkkkkkkkkk");
 		r->send_sw->lar=ntohl(pkt->ackno)-1;
-		send_deletenodes(r);
-		rel_read(r);
+		//fprintf(stderr,"eofseqno:%d, comp:%d",eofseqno,(ntohl(pkt->ackno)-1));
+		if (eofseqno==(ntohl(pkt->ackno)-1)) {
+			//fprintf(stderr,"destryyyyyyyy");
+			eofacked=1;
+			//rel_destroy(r);
+			return;
+		}
+		if (eofseqno==0) {
+			send_deletenodes(r);
+			rel_read(r);
+		}
 	}
 	else if (ntohs(pkt->len) == 12) {
-		rel_sendack(r);
-		conn_output(r->c, (void *)(r->receiverbuffer), 0);
+		rel_sendack(r,1);
+		//conn_output(r->c, (void *)(r->receiverbuffer), 0);
 		//rel_destroy(r);
 		return;
 	}
@@ -372,7 +392,7 @@ rel_recvpkt (rel_t *r, packet_t *pkt, size_t n)
 			//{
 				// all frames, even if higher number of packets have been received will be received and we send an ack
 				//not sure if seqNumToAck needs to be incremented
-			rel_sendack(r);
+			rel_sendack(r,0);
 			//}
 		}
 
@@ -388,7 +408,7 @@ rel_read (rel_t *s)
 	//fprintf(stderr,"\nrelreaddddddddddddddddddd\n");
 	if (windowfull!=NULL) {
 		if (iter_PackNAdd(windowfull, s) == 0) {
-			//fprintf(stderr, "head of send_sw: %s", s->send_sw->head->packet->data);
+			//fprintf(stderr, "windowfull resend: head of send_sw: %s", s->send_sw->head->packet->data);
 			return;
 		}
 		else {
@@ -397,9 +417,10 @@ rel_read (rel_t *s)
 		}
 	}
 	int r = conn_input(s->c, (void *)(s->senderbuffer), sizeof(char));
-	if (r == -1) {
-		fprintf(stderr, "eofffffffffffffffffffffff");
+	if (r == -1 && eofsent==0) {
+		//fprintf(stderr, "eofffffffffffffffffffffff");
 		rel_sendeof(s);
+		eofsent=1;
 		//rel_destroy(s);
 		return;
 	}
@@ -425,7 +446,7 @@ rel_read (rel_t *s)
 			s->acked = 0;
 			//fprintf(stderr, "\nsendpkt: %s\n", pack->data);
 			if (iter_PackNAdd(pack, s) == 0) {
-				//fprintf(stderr, "head of send_sw: %s", s->send_sw->head->packet->data);
+				//fprintf(stderr, "\nfull, head of send_sw: %s", s->send_sw->head->packet->data);
 				return;
 			}
 			////conn_sendpkt(s->c, pack, sizeof(packet_t));
@@ -440,12 +461,6 @@ rel_read (rel_t *s)
 			//s->packet = pack;
 		}
 	}
-	if (r == -1) {
-		fprintf(stderr, "eofffffffffffffffffffffffffffffffff2");
-		rel_sendeof(s);
-		//rel_destroy(s);
-		return;
-	}
 	//fprintf(stderr, "\nsendpkt: %s\n", pack->data);
 	//pack->data[dataindex]='\0';
 	pack->len = htons(12 + sizeof(char) * strlen(pack->data));
@@ -456,12 +471,19 @@ rel_read (rel_t *s)
 	//fprintf(stderr, "head of send_sw: %s", s->send_sw->head->packet->data);
 	//fprintf(stderr,"notfull %d",notfull);
 	if (notfull == 0) {
+		//fprintf(stderr, "\nfullfrag, head of send_sw: %s", s->send_sw->head->packet->data);
 		return;
 	}
 	//free(pack);
 	s->seqnum++;
 
-
+	if (r == -1 && eofsent==0) {
+		//fprintf(stderr, "eofffffffffffffffffffffffffffffffff2");
+		rel_sendeof(s);
+		eofsent=1;
+		//rel_destroy(s);
+		return;
+	}
 	//fprintf(stderr, "\n");
 }
 
@@ -469,13 +491,19 @@ void
 rel_output (rel_t *r)
 {
 	//fprintf(stderr, "reloutputtt\n");
-	conn_output(r->c, (void *)(r->receiverbuffer), sizeof(char));
+	//if (*(r->receiverbuffer)!=0) {
+	//fprintf(stderr, "%c",*(r->receiverbuffer));
+		conn_output(r->c, (void *)(r->receiverbuffer), sizeof(char));
+	//}
 
 }
 
 void
 rel_timer ()
 {
+	if (eofacked==1) {
+		return;
+	}
 	/* Retransmit any packets that need to be retransmitted */
 	//fprintf(stderr, "hello%s\n", rel_list->packet->data);
 	if(counter % 15 == 0)
